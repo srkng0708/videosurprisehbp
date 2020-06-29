03100e8eefb949d68e829dd019247ac85ebb830d
srkng0708 <51293510+srkng0708@users.noreply.github.com>
2020-06-30 02:52:42 +0530

    v 1.1.0 (Updated for next year)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 29, 2020").getTime();
+var countDownDate = new Date("Jun 29, 2021").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
